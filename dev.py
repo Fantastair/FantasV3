6e6000ebeca44238c7c79dfd623279d10f85e8d7
Fantastair <Fantastair@qq.com>
2026-02-20 13:40:53 +0800

    保存 format 命令产生的更改

--- dev.py
+++ dev.py
@@ -30,7 +30,6 @@ PYGAME_COMMIT_HASH_FILE = FANTAS_SOURCE_DIR / "_vendor" / "pygame" / "commit_has
 PYGAME_LOCK_HASH_FILE = CWD / "pygame.lock"
 
 
-
 class Colors(Enum):
     """
     Colors 枚举定义了用于终端输出的颜色代码。它提供了一种方便的方式来在终端中使用颜色，
@@ -342,9 +341,7 @@ def install_pygame_ce_for_fantas(py: Path, commit_hash: str | None) -> str:
     """
     commit_hash = checkout_pygame_ce_for_fantas(commit_hash)
 
-    pprint(
-        f"- 安装 pygame-ce for fantas ({commit_hash[:7]})"
-    )
+    pprint(f"- 安装 pygame-ce for fantas ({commit_hash[:7]})")
 
     delete_pygame_ce_for_fantas()
     pip_installed = True
@@ -366,7 +363,6 @@ def install_pygame_ce_for_fantas(py: Path, commit_hash: str | None) -> str:
 
     if not pip_installed:
         pprint("- 尝试使用 dev.py 安装", Colors.YELLOW)
-
 
         delete_file_or_dir(PYGAME_DIST_DIR)
         cmd_run(
@@ -403,8 +399,7 @@ def install_pygame_ce_for_fantas(py: Path, commit_hash: str | None) -> str:
     set_pygame_commit_hash(PYGAME_COMMIT_HASH_FILE, commit_hash)
 
     pprint(
-        f"  # pygame-ce for fantas (版本: {commit_hash[:7]})"
-        " 已安装",
+        f"  # pygame-ce for fantas (版本: {commit_hash[:7]})" " 已安装",
         Colors.GREEN,
     )
 
